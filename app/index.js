cd8a9e935e530850623bf679d4878c4006019976
AbuSufianIslam <thebooming1992@gmail.com>
2019-12-17 13:06:59 -0500

    First step done

---
+++ app/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store, { incrementCounter } from './store'; // imported for you already
+
+class Counter extends React.Component {
+	constructor() {
+		super();
+		this.state = store.getState();
+		//this.increment = this.increment.bind(this);
+	}
+
+	componentDidMount() {
+		this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	increment() {
+		store.dispatch(incrementCounter());
+	}
+
+	render() {
+		return (
+			<div id="container">
+				<div id="counter">
+					<h1>{this.state.count}</h1>
+					<button onClick={this.increment}>Increment</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
---
+++ app/store.js
@@ -0,0 +1,29 @@
+import { createStore } from 'redux';
+
+// Still need:
+//  - action type
+//  - action creator
+//  - initial state
+//  - reducer
+
+const initialState = {
+	// what should be on our initial state?
+	count: 0
+};
+
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+
+export const incrementCounter = () => ({ type: INCREMENT_COUNTER });
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case INCREMENT_COUNTER:
+			return { ...state, count: state.count + 1 };
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+
+export default store;
